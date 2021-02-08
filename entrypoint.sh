aa1e13555460823c4b097f09252c6cac2a69e055
Yevgeny Pats <yev.pats@gmail.com>
2021-02-08 11:53:29 +0200

    Bufix in gpg sign
    https://github.com/keybase/keybase-issues/issues/2798

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 if [[ -z "${PRIVATE_KEY}" ]]; then
     echo "can not find any private key, ignore..."
 else
+    export GPG_TTY=$(tty)
     key_file=$HOME/key.asc
     echo -e "${PRIVATE_KEY}" | base64 -d >"$key_file"
     echo "save key to $key_file"
 
-    if gpg --allow-secret-key-import --import "${key_file}"; then
+    if gpg --batch --allow-secret-key-import --import "${key_file}"; then
         gpg --list-secret-keys --keyid-format long
     fi
     rm -rf "$key_file"
